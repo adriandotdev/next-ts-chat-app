d5bfb177b9bcac89717d28df3d38f30f18256585
Adrian Nads Marcelo <marcelonads@gmail.com>
2024-11-30 15:17:12 +0800

    development: Added API call in Login form

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,50 +1,100 @@
 import React from "react";
-
+import { useForm, SubmitHandler } from "react-hook-form";
+import { LoginInput } from "@/types/Inputs";
+import axios, { AxiosError } from "axios";
+import Response from "@/types/Response";
 function LoginForm() {
+	const {
+		register,
+		handleSubmit,
+		setError,
+		formState: { errors, isSubmitting },
+		reset,
+		setFocus,
+	} = useForm<LoginInput>();
+
+	const API_URL = String(process.env.NEXT_PUBLIC_API_URL);
+
+	const Login: SubmitHandler<LoginInput> = async (data) => {
+		try {
+			const result = await axios.post(`${API_URL}/api/v1/accounts/login`, {
+				username: data.username,
+				password: data.password,
+			});
+
+			if (result.status === 200) {
+				reset();
+			}
+
+			console.log(result);
+		} catch (err) {
+			let error = err as AxiosError;
+			let data = error.response?.data as Response;
+
+			if (data.message === "INVALID_CREDENTIALS") {
+				setError("username", { message: "Invalid credentials" });
+				setError("password", { message: "Invalid credentials" });
+				setFocus("username", { shouldSelect: true });
+			}
+		}
+	};
+
 	return (
 		<form
 			className="flex flex-col max-w-[30rem] w-full justify-center items-center gap-4 mt-4"
-			action=""
+			onSubmit={handleSubmit(Login)}
 		>
 			{/* Username Field */}
 			<section className="w-full">
 				<label
-					className="label label-text text-lg font-semibold"
+					className="label label-text text-base font-semibold"
 					htmlFor="username"
 				>
 					Username
 				</label>
 				<input
-					className="w-full input input-primary"
+					className={`w-full input input-primary ${
+						errors.username?.message && "input-error"
+					}`}
 					type="text"
-					name="username"
-					id="username"
-					placeholder="Please enter your username"
+					id="given-name"
+					placeholder="Please enter your given name"
+					{...register("username", {
+						required: "Please provide your username",
+					})}
 				/>
+				<small className="text-red-700">
+					{errors.username?.message && errors.username.message}
+				</small>
 			</section>
 
 			{/* Password Field */}
 			<section className="w-full">
 				<label
-					className="label label-text text-lg font-semibold"
+					className="label label-text text-base font-semibold"
 					htmlFor="password"
 				>
 					Password
 				</label>
 				<input
-					className="w-full input input-primary"
+					className={`w-full input input-primary ${
+						errors.password?.message && "input-error"
+					}`}
 					type="password"
-					name="password"
-					id="password"
-					placeholder="Please enter your password"
+					id="given-name"
+					placeholder="Please enter your given name"
+					{...register("password", {
+						required: "Please provide your password",
+					})}
 				/>
+				<small className="text-red-700">
+					{errors.password?.message && errors.password.message}
+				</small>
 			</section>
 
-			<input
-				type="submit"
-				value="Login"
-				className="btn btn-primary w-full mt-3"
-			/>
+			<button className="btn btn-primary w-full mt-3">
+				{isSubmitting ? "Loading..." : "Sign In"}
+			</button>
 		</form>
 	);
 }
